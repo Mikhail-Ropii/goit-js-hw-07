8481049257cfdeb40055194487183232216c7451
Mikhail-Ropii <mr.ropiy@gmail.com>
2021-12-11 19:42:44 +0200

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
 function createGalleryMarkup(items) {
-    return galleryItems.map(({ preview, original, description }) => {
+    return items.map(({ preview, original, description }) => {
         return `
         <div class="gallery__item">
             <a class="gallery__link" href="${original}">
@@ -19,4 +23,21 @@ function createGalleryMarkup(items) {
     }).join('');
 };
 
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+    const imgSrc = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src=${imgSrc} width="800" height="600">
+`)
+    instance.show();
+}
+
+
+
 console.log(createGalleryMarkup);
+console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        `
+    }).join('');
+};
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250});
+
+}
+
+
+
+console.log(createGalleryMarkup);
+console.log(galleryItems);
+
 console.log(galleryItems);
